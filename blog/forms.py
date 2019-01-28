29ae057f996b5c69f700e9ac708057e66f523bb1
prime7 <tarek5701@gmail.com>
2019-01-28 12:54:00 +0600

    First Commit

---
+++ blog/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Post
+from django_summernote.widgets import SummernoteWidget,SummernoteInplaceWidget
+
+class PostCreateForm(forms.Form):
+    title = forms.CharField(max_length=100)
+    content = forms.CharField(widget=SummernoteWidget())
+
+class PostCreateForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
+        widgets = {
+            'content': SummernoteWidget(attrs={'summernote': {'width': '100%', 'height': '500px'}}),
+        }
---
+++ blog/views.py
@@ -0,0 +1,93 @@
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from .models import Post
+from .forms import PostCreateForm
+from django.db.models import Q
+
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 5
+
+
+    def get(self, request, *args, **kwargs):
+        if request.GET:
+            posts = Post.objects.all()
+            query = request.GET["q"]
+            if query:
+                posts = posts.filter(Q(title__icontains=query)|Q(content__icontains=query)).distinct() # author, author_id, content, date_posted, id, title
+                return render(request, self.template_name, {'posts': posts})
+        return super().get(request, *args, **kwargs)
+
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostCreateForm
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
