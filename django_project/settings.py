299883dc505ec77fa40a5452970486cf54fa2bd3
prime7 <tarek5701@gmail.com>
2019-02-14 02:15:40 +0600

    Postgresql

--- django_project/settings.py
+++ django_project/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER_NAME'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOSTNAME'),
+        'PORT': config('DB_PORT'),
     }
 }
 
